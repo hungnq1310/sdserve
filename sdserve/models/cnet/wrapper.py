132a9833761e0a21875f6e6122234354d4596d6a
hungnq1310 <hungnq131002gmail.com>
2024-10-14 15:39:34 +0700

    chores: fix output tracing, change to use optional

--- sdserve/models/cnet/wrapper.py
+++ sdserve/models/cnet/wrapper.py
@@ -1,4 +1,5 @@
 import torch
+from typing import Optional
 from diffusers import ControlNetModel
 
 class ControlNetWrapper(torch.nn.Module):
@@ -14,7 +15,7 @@ class ControlNetWrapper(torch.nn.Module):
     - sdxl: https://github.com/huggingface/diffusers/blob/v0.30.3/src/diffusers/pipelines/controlnet/pipeline_controlnet.py#L1456 (line L1456 to 1486)
     - sd15: https://github.com/huggingface/diffusers/blob/v0.30.3/src/diffusers/pipelines/controlnet/pipeline_controlnet.py#1246 (line 1246 to 1272)
     """
-    def __init__(self, model_name_or_path: str, attention_slicing: str | int | None) -> None:
+    def __init__(self, model_name_or_path: str, attention_slicing: Optional[str] = None) -> None:
         super().__init__()
         self.controlnet = ControlNetModel.from_pretrained(model_name_or_path)
         if attention_slicing is not None:
@@ -27,8 +28,8 @@ class ControlNetWrapper(torch.nn.Module):
         prompt_embeds: torch.Tensor,        # positional argument
         cond_image: torch.Tensor,           # positional argument
         cond_scale: torch.Tensor,           # positional argument
-        add_text_embeds: torch.Tensor | None,      # use for xl
-        add_time_ids: torch.Tensor | None,         # use for xl
+        add_text_embeds: Optional[torch.Tensor] = None,      # use for xl
+        add_time_ids: Optional[torch.Tensor] = None,         # use for xl
         ):
         # controlnet(s) inference
         added_cond_kwargs = {}
@@ -46,4 +47,4 @@ class ControlNetWrapper(torch.nn.Module):
             added_cond_kwargs=added_cond_kwargs if added_cond_kwargs else None,
             return_dict=False,
         )
-        return down_block_res_samples, mid_block_res_sample
+        return down_block_res_samples + [mid_block_res_sample] # return list instead of tuple(list, non-list)
