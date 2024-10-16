cf0ba2311f765c1311eb159e2f6ad13593729df4
hungnq1310 <hungnq131002gmail.com>
2024-10-16 16:59:19 +0700

    chores: test with 1 model first

--- cnext_convert.sh
+++ cnext_convert.sh
@@ -1,5 +1,5 @@
 python3 -u sdserve/scripts/cnextxl_converter.py \
-    --ckpt-path "models/pretrained/ControlAny-SDXL/vidit_depth/controlnet.safetensors" \
+    --ckpt-path "models/controlnet.safetensors" \
     --output-path models \
     --opset 14 \
     --is-sdxl
