4c76025d940faa4798bd182d1b3ba85d6d7e3feb
hungnq1310 <hungnq131002gmail.com>
2024-10-16 16:35:52 +0700

    fix: change path save folder

--- cnext_convert.sh
+++ cnext_convert.sh
@@ -1,5 +1,5 @@
 python3 -u sdserve/scripts/cnextxl_converter.py \
-    --ckpt-path "/models/pretrained/ControlAny-SDXL/vidit_depth/controlnet.safetensors" \
-    --output-path /models \
+    --ckpt-path "models/pretrained/ControlAny-SDXL/vidit_depth/controlnet.safetensors" \
+    --output-path models \
     --opset 14 \
     --is-sdxl
